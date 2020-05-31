3623a4e21c1ce789b6d5baf52dd91130872b92dc
sido1271 <mariusz.matyasik@outlook.com>
2020-05-31 20:18:31 +0200

    Delete sysout in constructor

--- src/com/gmail/kursspring/PrintServiceImpl.java
+++ src/com/gmail/kursspring/PrintServiceImpl.java
@@ -3,7 +3,6 @@ package com.gmail.kursspring;
 public class PrintServiceImpl implements PrintService {
 	
 	public PrintServiceImpl() {
-		System.out.println("Inside no-arg constructor - PrintServiceImpl");
 	}
 	
 	@Override
