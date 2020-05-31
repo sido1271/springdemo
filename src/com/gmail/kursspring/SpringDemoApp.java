2e8bd801f02d923e9321c302d98bd86e09709517
sido1271 <mariusz.matyasik@outlook.com>
2020-05-31 20:16:15 +0200

    Update main to check app functionality

--- src/com/gmail/kursspring/SpringDemoApp.java
+++ src/com/gmail/kursspring/SpringDemoApp.java
@@ -10,13 +10,8 @@ public class SpringDemoApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		Company company = context.getBean("myRenovationCompany", Company.class);
-		Specialist decorSpec = context.getBean("myDecorator", Specialist.class);
-		Specialist electricSpec = context.getBean("myElectrician", Specialist.class);
 		
 		company.printInformations();
-		System.out.println(decorSpec.introduceSpecialist());
-		System.out.println(electricSpec.introduceSpecialist());
-		
 		company.printPrice();
 		company.printDate();
 		
